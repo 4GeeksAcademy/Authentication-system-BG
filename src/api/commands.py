889decf2065b7ff5f9353c1028637850114ab619
BernardoGSch-prog <127693607+BernardoGSch-prog@users.noreply.github.com>
2023-11-04 15:44:37 +0000

    Finish

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,4 +1,3 @@
-
 import click
 from api.models import db, User
 
@@ -14,21 +13,17 @@ def setup_commands(app):
     by typing: $ flask insert-test-users 5
     Note: 5 is the number of users to add
     """
-    @app.cli.command("insert-test-users") # name of our command
-    @click.argument("count") # argument of out command
-    def insert_test_users(count):
+    @app.cli.command("insert-test-users")
+    @click.argument("count") 
+    def insert_test_data(count):
         print("Creating test users")
         for x in range(1, int(count) + 1):
             user = User()
             user.email = "test_user" + str(x) + "@test.com"
-            user.password = "123456"
+            user.password = "12345"
             user.is_active = True
             db.session.add(user)
             db.session.commit()
             print("User: ", user.email, " created.")
 
         print("All test users created")
-
-    @app.cli.command("insert-test-data")
-    def insert_test_data():
-        pass
